5593b26515f39f4212fadafc26b6ba4fb4a79b0b
Juste Kavaliauskaite <justekavaliauskaite2@gmail.com>
2021-11-08 13:53:10 +0200

    [4] - adding get and delete methods

---
+++ js/API.js
@@ -0,0 +1,28 @@
+const urlBase = "http://localhost:3000";
+
+class API {
+  static getToys = (resolve, reject) => {
+    fetch(urlBase + "/toys")
+      .then((data) => data.json())
+      .then(resolve)
+      .catch(reject);
+  };
+
+  static deleteToys = (id, resolve, reject) => {
+    fetch(urlBase + "/toys" + "/" + id, { method: "DELETE" })
+      .then((data) => data.json())
+      .then(resolve)
+      .catch(reject);
+  };
+}
+
+// API.getToys(
+//   (duomenys) => console.log("Gauti duomenys: ", duomenys),
+//   (klaida) => console.error("Kode yra klaida: ", klaida)
+// );
+
+// API.deleteToys(
+//   "2",
+//   (duomenys) => console.log("Gauti duomenys: ", duomenys),
+//   (klaida) => console.error("Kode yra klaida: ", klaida)
+// );
